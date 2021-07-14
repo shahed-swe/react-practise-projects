5abb110297485c40a770a8d9d0fc509c87068476
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-07-14 21:33:49 +0600

    another commit:

--- clickcounter/src/App.jsx
+++ clickcounter/src/App.jsx
@@ -1,17 +1,29 @@
+import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
+import {useState} from 'react';
 
 function App() {
 
   const [count, setCount] = useState(0);
 
-
   return (
     <div className="App">
-        <h1>Count: {count}</h1>
-        <button onClick={() => setCount(count-1)}>Decreate</button>
-        <button onClick={() => setCount(count+1)}>Increate</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Count: {count}</h1>
+          <button onClick={() => setCount(count-1)}>Decreate</button>
+          <button onClick={() => setCount(count+1)}>Increate</button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
